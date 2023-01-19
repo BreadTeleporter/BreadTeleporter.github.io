777f683e65f29275d82a088ee0babe06f9d3d1c8
BreadTeleporter <103989916+BreadTeleporter@users.noreply.github.com>
2023-01-19 12:00:25 +0000

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+function page(divname, who){
+    const pages = document.querySelectorAll('.page');
+    pages.forEach(page => {
+        page.style.display = "none"
+    });
+    document.getElementById(divname).style.display = "block"
+
+    const activea = document.querySelectorAll('.active');
+    activea.forEach(active => {
+        active.className = ""
+    });
+    document.getElementById(who).className = "active"
+}
