a0baef86acf7d2dd11fec219087fce734f9a074c
BreadTeleporter <103989916+BreadTeleporter@users.noreply.github.com>
2023-05-03 20:30:39 +0100

    Added redirect to new site 🎉

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
-function page(divname, who){
-    const pages = document.querySelectorAll('.page');
-    pages.forEach(page => {
-        page.style.display = "none"
-    });
-    document.getElementById(divname).style.display = "block"
+window.location = "breadteleporter.duckdns.org";
 
-    const activea = document.querySelectorAll('.active');
-    activea.forEach(active => {
-        active.className = ""
-    });
-    document.getElementById(who).className = "active"
-}
+// function page(divname, who){
+//     const pages = document.querySelectorAll('.page');
+//     pages.forEach(page => {
+//         page.style.display = "none"
+//     });
+//     document.getElementById(divname).style.display = "block"
+
+//     const activea = document.querySelectorAll('.active');
+//     activea.forEach(active => {
+//         active.className = ""
+//     });
+//     document.getElementById(who).className = "active"
+// }
